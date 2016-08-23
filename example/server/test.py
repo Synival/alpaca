028a2634afb17e0d2c3a13bba5b70ec0757b7f27
Simon Bielman <simon@baroque-creations.com>
2016-08-23 13:42:11 -0700

    Placeholder tests for webserver / JavaScript tests.

---
+++ example/server/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# Enable debugging.
+import cgitb
+cgitb.enable()
+
+print "Content-Type: text/HTML; charset=utf-8"
+print
+print "<html>"
+print "<body>"
+print
+print "Hello World!"
+print
+print "</html>"
+print "</body>"
